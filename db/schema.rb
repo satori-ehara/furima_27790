13f28d810acb3e3cbf522832d6a75e869a40e29c
satori-ehara <satori.0701191@gmail.com>
2020-07-20 15:08:41 +0900

    form_withのデータの送信先をmodel: @user, url: user_registration_pathに設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_031807) do
+ActiveRecord::Schema.define(version: 2020_07_20_044949) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 2020_07_20_031807) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.string "Birthday"
+    t.string "first_name_kanji"
+    t.string "last_name_kanji"
+    t.string "first_name_katakana"
+    t.string "last_name_katakana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
