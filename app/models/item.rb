ba0fc8d7dff66abca8921a4e5136db02e7c19fc2
satori-ehara <satori.0701191@gmail.com>
2020-07-21 18:53:05 +0900

    各種対応するモデルと紐付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :type
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecture
   belongs_to_active_hash :day
 
 end
