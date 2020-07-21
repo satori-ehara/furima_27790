3b280a0e52a8f8adf9d7508bbf2fc90e974b4fe6
satori-ehara <satori.0701191@gmail.com>
2020-07-21 12:38:24 +0900

    バリデーションを設定（名前を平仮名片仮名漢字のみ、読みをカタカナのみ、生年月日を入力しなかったらダメ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :first_name_katakana, presence: true,format: { with: /^([ァ-ン]|ー)+$/ }
+    validates :first_name_katakana,:last_name_katakana, presence: true,format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
+    validates :Birthday_year,:Birthday_month,:Birthday_day, presence: true,format: { without: /[0]/ }
 end
