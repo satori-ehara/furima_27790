e2a8539a6cd758f55e96f07332e3ae2b7bd7e4b0
satori-ehara <satori.0701191@gmail.com>
2020-07-21 16:55:21 +0900

    rubocopの実装（自動修正）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,16 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
   def configure_permitted_parameters
-    if params[:user] != nil
-      params[:user][:Birthday_year] = params[:user]["Birthday(1i)"].to_i
-      params[:user][:Birthday_month] = params[:user]["Birthday(2i)"].to_i
-      params[:user][:Birthday_day] = params[:user]["Birthday(3i)"].to_i
+    unless params[:user].nil?
+      params[:user][:Birthday_year] = params[:user]['Birthday(1i)'].to_i
+      params[:user][:Birthday_month] = params[:user]['Birthday(2i)'].to_i
+      params[:user][:Birthday_day] = params[:user]['Birthday(3i)'].to_i
     end
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:Birthday_year,:Birthday_month,:Birthday_day,:first_name_kanji,:last_name_kanji,:first_name_katakana,:last_name_katakana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :Birthday_year, :Birthday_month, :Birthday_day, :first_name_kanji, :last_name_kanji, :first_name_katakana, :last_name_katakana])
   end
 end
