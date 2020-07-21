7f71c5b1e4c3749030bbf86a1697fff363e21b4c
satori-ehara <satori.0701191@gmail.com>
2020-07-21 15:35:17 +0900

    emailに@が必要である事と同じメールアドレスは登録できない事のテストを追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,18 @@ describe User do
       expect(user.errors[:password]).to include("can't be blank")
     end
 
+    it "email need to @" do
+      user = build(:user, email: "asdasdasd")
+      user.valid?
+      expect(user.errors[:email]).to include("is invalid")
+    end
+
+    it "email is only one" do
+      user = build(:user)
+      user_2 = build(:user)
+      user_2.valid?
+      expect(user.errors[:email]).to include()
+    end
+
   end
 end
