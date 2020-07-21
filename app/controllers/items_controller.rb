e55f4f29593626168b35ac002742010334ed5db9
satori-ehara <satori.0701191@gmail.com>
2020-07-21 19:26:02 +0900

    itemsコントローラにてストロングパラメータを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    @items = items.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:items).permit(:name,:description,:image,:price,:type_id,:burden_id,:prefectures_id,:days_id,:condition_id).merge(user_id: current_user.id)
+  end
+
 end
