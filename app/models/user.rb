42e1c4bdef3f025ee2c2ec41f2493ab9a40db460
satori-ehara <satori.0701191@gmail.com>
2020-07-21 16:54:13 +0900

    Birthday関係のバリデーションが、0である時（入力がされていなかった場合になる）にしたかったが、0を含む時になってしまっていた問題を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
     validates :password, presence: true,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i , message: "need 1~9&a~z"}
     validates :first_name_kanji,:last_name_kanji, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :first_name_katakana,:last_name_katakana, presence: true,format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/ }
-    validates :Birthday_year,:Birthday_month,:Birthday_day, presence: true,format: { without: /[0]/ }
+    validates :Birthday_year,:Birthday_month,:Birthday_day, presence: true,format: { without: /\A0/ }
 end
