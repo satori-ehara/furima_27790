7fc7754a6ccb6f99beba27e5a262b09cbcbfef2f
satori-ehara <satori.0701191@gmail.com>
2020-07-22 14:35:15 +0900

    ログインしていないと出品できないように変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :check_login, only: [:new,:create]
+
   def index
     @items = Item.all
   end
 
   def new
+
   end
 
   def create
@@ -19,6 +22,12 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.permit(:name,:description,:image,:price,:type_id,:burden_id,:prefectures_id,:days_id,:condition_id).merge(user_id: current_user.id)
+  end
+
+  def check_login
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
