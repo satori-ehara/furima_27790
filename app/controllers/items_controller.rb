5f8d4f6870a8c23aaace164f5011a33b6a2f0c93
satori-ehara <satori.0701191@gmail.com>
2020-07-23 12:12:16 +0900

    データベースの変更に伴いストロングパラメータを変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :image, :price, :type_id, :burden_id, :prefectures_id, :days_id, :condition_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :image, :price, :type_id, :burden_id, :prefecture_id, :day_id, :condition_id).merge(user_id: current_user.id)
   end
 
   def check_login
