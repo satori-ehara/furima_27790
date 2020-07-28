c120caf410acfbb6f0752c2b0eea288c79c34401
satori-ehara <satori.0701191@gmail.com>
2020-07-28 12:50:14 +0900

    addressesテーブル作成

--- db/migrate/20200727025236_create_buys.rb
+++ db/migrate/20200727025236_create_buys.rb
@@ -3,12 +3,6 @@ class CreateBuys < ActiveRecord::Migration[6.0]
     create_table :buys do |t|
       t.integer :user_id
       t.integer :item_id
-      t.integer :post_code
-      t.integer :prefectures_id
-      t.string  :city
-      t.string  :address
-      t.string  :building
-      t.integer :phone_number
 
 
       t.timestamps
---
+++ db/migrate/20200728034656_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.integer :post_code
+      t.integer :prefectures_id
+      t.string  :city
+      t.string  :address
+      t.string  :building
+      t.integer :phone_number
+      t.integer :item_id
+
+      t.timestamps
+    end
+  end
+end
