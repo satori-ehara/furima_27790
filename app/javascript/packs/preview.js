2792f18967c77f02e86b0d6cf0e1e7bbb23fd663
satori-ehara <satori.0701191@gmail.com>
2020-08-20 18:26:08 +0900

    商品出品の際に画像を選択したら、下に選択した画像が憑依されるように変更

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list')
+  document.getElementById('item_image').addEventListener('change', function(e){
+    console.log(e);
+    const file = e.target.files[0]
+    const blob = window.URL.createObjectURL(file);
+    blob.width = 100;
+
+    // 画像を表示するためのdiv要素を生成
+    const imageElement = document.createElement('div')
+
+    // 表示する画像を生成
+    const blobImage = document.createElement('img')
+    blobImage.setAttribute('src', blob)
+    blobImage.width = 100;
+    imageElement.appendChild(blobImage)
+    ImageList.appendChild(imageElement)
+  });
+});
