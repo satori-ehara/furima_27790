56fc589e5157c5b2765f2a382c5c2e5205ee1cb1
satori-ehara <satori.0701191@gmail.com>
2020-08-24 13:37:32 +0900

    画像投稿機能の拡張の微修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,21 +1,19 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('DOMContentLoaded', () => {
     const ImageList = document.getElementById('image-list')
-    document.getElementById('item_image').addEventListener('change', function(e){
-      const file = e.target.files[0]
-      const blob = window.URL.createObjectURL(file);
-      blob.width = 100;
 
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
       // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div')
       imageElement.setAttribute('id', "image-element")
       let imageElementNum = document.querySelectorAll('#image-element').length
 
-
       // 表示する画像を生成
       const blobImage = document.createElement('img')
-      blobImage.setAttribute('src', blob)
       blobImage.width = 100;
+      blobImage.setAttribute('src', blob)
+
       // ファイル選択ボタンを生成
       const inputHTML = document.createElement('input')
       inputHTML.setAttribute('id', `item_image_${imageElementNum}`)
@@ -26,6 +24,21 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       imageElement.appendChild(blobImage)
       imageElement.appendChild(inputHTML)
       ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById('item_image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+
     });
   });
 }
